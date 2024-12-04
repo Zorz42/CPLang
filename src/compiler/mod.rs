c201ba3946c88c62084201982add09fbdf389d17
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-12-04 15:54:49 +0100

    added a basic tokenizer

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,3 +1,29 @@
-pub fn compile(input_file: &str, output_file: &str) {
+use crate::compiler::tokenizer::parse_blocks;
 
+mod tokenizer;
+
+// splits input into lines and parses indentation levels
+fn parse_indentation(input: &str) -> Vec<(i32, String)> {
+    let mut lines = Vec::new();
+    for line in input.lines() {
+        let mut indent = 0;
+        let leading_spaces = line.chars().take_while(|c| *c == ' ').count();
+        if leading_spaces % 4 != 0 {
+            panic!("Invalid indentation");
+        }
+
+        indent = (leading_spaces / 4) as i32;
+        lines.push((indent, line.trim().to_string()));
+    }
+    lines
+}
+
+pub fn compile(input_file: &str, output_file: &str) {
+    // read input file into a string
+    let input = std::fs::read_to_string(input_file).unwrap();
+
+    let lines = parse_indentation(&input);
+    let master_block = parse_blocks(lines);
+
+    println!("{:?}", master_block);
 }
---
+++ src/compiler/tokenizer.rs
@@ -0,0 +1,159 @@
+#[derive(Debug)]
+enum Keyword {
+    If,
+    Else,
+    While,
+    For,
+    Fn,
+}
+
+impl Keyword {
+    fn from_str(s: &str) -> Option<Keyword> {
+        match s {
+            "if" => Some(Keyword::If),
+            "else" => Some(Keyword::Else),
+            "while" => Some(Keyword::While),
+            "for" => Some(Keyword::For),
+            "fn" => Some(Keyword::Fn),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Symbol {
+    Plus,
+    LeftBracket,
+    RightBracket,
+    Equals,
+}
+
+impl Symbol {
+    fn from_char(c: char) -> Option<Symbol> {
+        match c {
+            '+' => Some(Symbol::Plus),
+            '(' => Some(Symbol::LeftBracket),
+            ')' => Some(Symbol::RightBracket),
+            '=' => Some(Symbol::Equals),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Constant {
+    Integer(i32),
+    Float(f32),
+    String(String),
+    Boolean(bool),
+}
+
+// a block is usually in an if statement, while loop, or function
+// the whole program is also a block
+#[derive(Debug)]
+pub struct Block {
+    children: Vec<Token>,
+}
+
+#[derive(Debug)]
+enum Token {
+    Keyword(Keyword),
+    Identifier(String),
+    Symbol(Symbol),
+    Constant(Constant),
+    Block(Block),
+}
+
+fn string_to_token(string: &str) -> Token {
+    if let Some(keyword) = Keyword::from_str(string) {
+        return Token::Keyword(keyword);
+    }
+
+    if let Ok(integer) = string.parse::<i32>() {
+        return Token::Constant(Constant::Integer(integer));
+    }
+
+    if let Ok(float) = string.parse::<f32>() {
+        return Token::Constant(Constant::Float(float));
+    }
+
+    if string.starts_with("\"") && string.ends_with("\"") {
+        return Token::Constant(Constant::String(string.to_string()));
+    }
+
+    if string == "true" || string == "false" {
+        return Token::Constant(Constant::Boolean(string == "true"));
+    }
+
+    Token::Identifier(string.to_string())
+}
+
+// turns a line into tokens
+fn parse_line(line: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut curr_token = String::new();
+
+    for c in line.chars() {
+        if Symbol::from_char(c).is_some() {
+            if curr_token.len() > 0 {
+                tokens.push(string_to_token(&curr_token));
+                curr_token.clear();
+            }
+            tokens.push(Token::Symbol(Symbol::from_char(c).unwrap()));
+        } else if c == ' ' {
+            if curr_token.len() > 0 {
+                tokens.push(string_to_token(&curr_token));
+                curr_token.clear();
+            }
+        } else {
+            curr_token.push(c);
+        }
+    }
+
+    if curr_token.len() > 0 {
+        tokens.push(string_to_token(&curr_token));
+    }
+
+    tokens
+}
+
+// parses one block
+fn parse_block(lines: &Vec<(i32, String)>, curr_idx: &mut usize) -> Block {
+    let mut block = Block {
+        children: Vec::new(),
+    };
+
+    let curr_ident = lines[*curr_idx].0;
+    let mut curr_token = String::new();
+
+    while *curr_idx < lines.len() {
+        let ident = lines[*curr_idx].0;
+
+        if ident == curr_ident {
+            let tokens = parse_line(&lines[*curr_idx].1);
+            for i in tokens {
+                block.children.push(i);
+            }
+
+        } else if ident == curr_ident + 1 {
+            let child_block = parse_block(lines, curr_idx);
+            block.children.push(Token::Block(child_block));
+
+        } else if ident == curr_ident - 1 {
+            return block;
+
+        } else {
+            panic!("Invalid indentation");
+        }
+
+        *curr_idx += 1;
+    }
+
+    block
+}
+
+// parses indentation into blocks: a block is a list of lines with the same indentation level
+pub fn parse_blocks(lines: Vec<(i32, String)>) -> Block {
+    let mut idx = 0;
+    parse_block(&lines, &mut idx)
+}
